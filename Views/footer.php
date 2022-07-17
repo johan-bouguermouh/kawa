6a5ddc63e8cdec5902aa7dcbfc90ef05bfb090da
Johan <johan.bouguermouh@laplateforme.io>
2022-07-17 14:50:04 +0200

    Fix HomePage Ux

--- Views/footer.php
+++ Views/footer.php
@@ -1,9 +1,13 @@
 <footer class="footerAboutUs">
+    <nav>
     <a href="/kawa/">
         <h3>Accueil boutique</h3>
     </a>
+    </nav>
+    <section>
     <i class="fab fa-facebook-f"></i>
     <a target="_blank" href="https://github.com/boris-tikhomiroff/kawa"><i class="fa-brands fa-github"></i></a>
     <i class="fa-brands fa-instagram"></i>
+    </section>
     <p>Projet La plateforme par trois étudiants.</p>
 </footer>
