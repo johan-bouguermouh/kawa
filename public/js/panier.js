5f98400ac5607a9f813594b2d45cebfc1c889ff5
Johan <johan.bouguermouh@laplateforme.io>
2022-07-23 23:52:01 +0200

    fix slide -in and slide-out panier

--- public/js/panier.js
+++ public/js/panier.js
@@ -3,14 +3,12 @@ function openNav() {
     console.log('in open')
     // document.getElementById("mySidenav").style.width = "450px";
     document.getElementById("mySidenav").style.transform = "translateX(0px)";
-    document.getElementById("main").style.marginLeft = "450px";
   }
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
   function closeNav() {
     // document.getElementById("mySidenav").style.width = "450px";
     document.getElementById("mySidenav").style.transform = "translateX(450px)";
-    document.getElementById("main").style.marginLeft = "450px";
   }
 
   window.addEventListener("DOMContentLoaded", () => {
